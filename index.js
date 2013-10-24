a198dece17364a3c95899cd4f37fe95b6ecc323b
Myles Byrne <myles@myles.id.au>
2013-10-24 14:38:21 -0700

    First implementation and tests

---
+++ index.js
@@ -0,0 +1,63 @@
+/*
+
+By default, the first arg of every fn in the list will be a function you can
+use to call the next function in the list:
+
+    var f = chain(
+      [ function(next, arr) { arr.push('a') ; next(arr) }
+      , function(next, arr) { arr.push('b') ; next(arr) }
+      ])
+    f([]) // => ['a','b']
+
+You can also pass 'bound' as the second arg to use `this.next()` to invoke the
+next function in the list:
+
+    var f = chain(
+      [ function(arr) { arr.push('a') ; this.next(arr) }
+      , function(arr) { arr.push('b') ; this.next(arr) }
+      ])
+    f([]) // => ['a','b']
+
+*/
+
+var ns = module.exports = function(fns, mode) {
+  mode = mode || 'arg'
+  switch (mode) {
+    case 'arg':   return ns.arg(fns)
+    case 'bound': return ns.bound(fns)
+    default: throw new Error(mode + ' is not a valid mode')
+  }
+}
+
+// From an array of functions return a function that is the first element of
+// the array with its `this` bound to an object who's `next` property is
+// the second function of the array with its bound to an object who's `next`
+// property is the third function in the array ... etc
+ns.bound = function(fns){
+  this._validateArgs(fns)
+
+  var head = fns[0], rest = fns.slice(1), recurse = ns.bound.bind(this)
+  if (1 === fns.length) return head.bind(null)
+  return head.bind({ next: recurse(rest) })
+}
+
+// From an array of functions (where the first arg of each function is a "next"
+// function) produces a single function that when invoked will invoke the
+// first function in the array passing a function that will invoke the second
+// function in the array (... etc) as the "next" argument
+ns.arg = function(fns) {
+  this._validateArgs(fns)
+
+  var head = fns[0], rest = fns.slice(1), recurse = ns.arg.bind(this)
+  if (0 === rest.length)
+    return function() { head.bind(null,null).apply(null,arguments) }
+  return function() {
+    head.bind(null,recurse(rest)).apply(null,arguments)
+  }
+}
+
+// Args must be a non-empty array
+ns._validateArgs = function(fns) {
+  if (!( fns && fns.length && fns.length !== 0 ))
+    throw new Error("Argument must be an array of functions")
+}
---
+++ test.js
@@ -0,0 +1,76 @@
+var chain  = require('./')
+  , assert = require('assert')
+
+// can't chain empty array
+assert.throws(function() { chain([]) })
+// can't chain nothing
+assert.throws(function() { chain() })
+
+// Chain one function
+var chainedFn = chain(
+  [ function(x) { console.log(1); assert('x' === x); assert(!this.next) }
+  ], 'bound')
+
+chainedFn('x')
+
+console.log('---')
+
+// Two
+var chainedFn = chain(
+  [ function(x) {
+      console.log(1); assert('x' === x); assert(this.next) ; this.next(x) }
+  , function(x) {
+      console.log(2); assert('x' === x); assert(!this.next) }
+  ], 'bound')
+
+chainedFn('x')
+
+console.log('---')
+
+// Three!
+var chainedFn = chain(
+  [ function(x) {
+      console.log(1); assert('x' === x); assert(this.next) ; this.next(x) }
+  , function(x) {
+      console.log(2); assert('x' === x); assert(this.next) ; this.next(x) }
+  , function(x) {
+      console.log(3); assert('x' === x); assert(!this.next) }
+  ], 'bound')
+
+chainedFn('x')
+
+console.log('---')
+
+// Arg style
+
+// Chain one function
+var chainedFn = chain(
+  [ function(next,x) { console.log(1); assert('x' === x); assert(!next) }
+  ], 'arg')
+
+chainedFn('x')
+
+console.log('---')
+
+// Two
+var chainedFn = chain(
+  [ function(next,x) { console.log(1); assert('x' === x); next(x) }
+  , function(next,x) { console.log(2); assert('x' === x); assert(!next) }
+  ], 'arg')
+
+chainedFn('x')
+
+console.log('---')
+
+// Three
+var chainedFn = chain(
+  [ function(next,x) { console.log(1); assert('x' === x); next(x) }
+  , function(next,x) { console.log(2); assert('x' === x); next(x) }
+  , function(next,x) { console.log(3); assert('x' === x); assert(!next) }
+  ], 'arg')
+
+chainedFn('x')
+
+console.log('---')
+
+console.log('ok')
